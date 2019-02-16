94d2fcbe7833a0db00380698d6b5434f9d24bc9c
Lucas Boyd <LucasBoydDev@gmail.com>
2019-02-16 12:22:45 -0500

    Swap header out with app bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,9 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 class Header extends Component {
   computeCaloriesLeft() {
@@ -16,9 +19,13 @@ class Header extends Component {
 
     return (
       <div className="header">
-        <Link to="/">
-          <h2 className="title">Calorie Tracker</h2>
-        </Link>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="h4" component={Link} to="/">
+              Calorie Tracker
+            </Typography>
+          </Toolbar>
+        </AppBar>
         <Table>
           <TableHead>
             <TableRow>
