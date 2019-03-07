9595b72cdb9b89cbf298441828b5ca3a334571e3
Lucas Boyd <LucasBoydDev@gmail.com>
2019-03-06 22:01:57 -0500

    Centers menu items

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
 import MenuItem from "@material-ui/core/MenuItem";
 import MenuList from "@material-ui/core/MenuList";
 
+const styles = theme => ({
+  menuItem: {
+    display: "block",
+    textAlign: "center"
+  }
+});
+
 const MainMenu = props => {
+  const { classes } = props;
+
   return (
     <MenuList>
-      <MenuItem component={Link} to="add-food">
+      <MenuItem component={Link} to="add-food" className={classes.menuItem}>
         Add Food
       </MenuItem>
-      <MenuItem component={Link} to="add-exercise">
+      <MenuItem component={Link} to="add-exercise" className={classes.menuItem}>
         Add Exercise
       </MenuItem>
-      <MenuItem component={Link} to="view-macros">
+      <MenuItem component={Link} to="view-macros" className={classes.menuItem}>
         View Macros
       </MenuItem>
-      <MenuItem component={Link} to="goals">
+      <MenuItem component={Link} to="goals" className={classes.menuItem}>
         Update Goals
       </MenuItem>
     </MenuList>
   );
 };
 
-export default MainMenu;
+export default withStyles(styles)(MainMenu);
